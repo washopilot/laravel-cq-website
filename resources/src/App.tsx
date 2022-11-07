73d7fd8d7d412e131f06703679c9e381b6085131
Fernando Chicaiza <washopilot@yahoo.com>
2022-11-07 06:27:54 -0500

    test-01

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -1,9 +1,35 @@
+import * as THREE from 'three';
+import React, { useRef, useState } from 'react';
+import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+
+function Box(props: ThreeElements['mesh']) {
+    const mesh = useRef<THREE.Mesh>(null!);
+    const [hovered, setHover] = useState(false);
+    const [active, setActive] = useState(false);
+    useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
+    return (
+        <mesh
+            {...props}
+            ref={mesh}
+            scale={active ? 2.5 : 1}
+            onClick={(event) => setActive(!active)}
+            onPointerOver={(event) => setHover(true)}
+            onPointerOut={(event) => setHover(false)}
+            >
+            <boxGeometry args={[1, 1, 1]} />
+            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
+        </mesh>
+    );
+}
+
 const App = () => {
     return (
-        <>
-            <h1>Hello world</h1>
-            <p>Esto es REACT</p>
-        </>
+        <Canvas>
+            <ambientLight />
+            <pointLight position={[10, 10, 10]} />
+            <Box position={[-1.2, 0, 0]} />
+            <Box position={[1.2, 0, 0]} />
+        </Canvas>
     );
 };
 
