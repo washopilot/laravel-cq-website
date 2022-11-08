fd08e569cd60880edde11d8af27a6e90f85404ac
Fernando Chicaiza <washopilot@yahoo.com>
2022-11-08 02:29:04 -0500

    modelos y selector

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -1,49 +1,38 @@
 import * as THREE from 'three';
 import React, { useRef, useState } from 'react';
-import { Canvas, useFrame, ThreeElements } from '@react-three/fiber';
+import { Canvas, useFrame, ThreeElements, PrimitiveProps } from '@react-three/fiber';
+import { useGLTF, OrbitControls, ContactShadows, Environment } from '@react-three/drei';
 import { Leva, useControls } from 'leva';
 
-const MODELS = {
-    Beech: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-beech/model.gltf',
-    Lime: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-lime/model.gltf',
-    Spruce: 'https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/tree-spruce/model.gltf'
+const MODELS: { [key: string]: string } = {
+    Percha: 'gltf/percha_02_03.gltf',
+    Gódola: 'gltf/gondola_01.gltf'
 };
 
-function Box(props: ThreeElements['mesh']) {
-    const mesh = useRef<THREE.Mesh>(null!);
-    const [hovered, setHover] = useState(false);
-    const [active, setActive] = useState(false);
-    useFrame((state, delta) => (mesh.current.rotation.x += 0.01));
-    return (
-        <mesh
-            {...props}
-            ref={mesh}
-            scale={active ? 2.5 : 1}
-            onClick={(event) => setActive(!active)}
-            onPointerOver={(event) => setHover(true)}
-            onPointerOut={(event) => setHover(false)}>
-            <boxGeometry args={[1, 1, 1]} />
-            <meshStandardMaterial color={hovered ? 'hotpink' : 'orange'} />
-        </mesh>
-    );
-}
-
 const App = () => {
-    const values = useControls({
-        model: { value: 'Beech', options: Object.keys(MODELS) }
+    const { model } = useControls({
+        model: { value: 'Percha', options: Object.keys(MODELS) }
     });
 
     return (
         <>
-            <Leva hideCopyButton />
-            <Canvas>
-                <ambientLight />
-                <pointLight position={[10, 10, 10]} />
-                <Box position={[-1.2, 0, 0]} />
-                <Box position={[1.2, 0, 0]} />
+            <Leva hideCopyButton titleBar={{ drag: false }} />
+            <Canvas camera={{ position: [100, 90, 200], fov: 50 }}>
+                <hemisphereLight color="white" groundColor="blue" intensity={0.75} />
+                {/* <spotLight position={[50, 50, 10]} angle={0.15} penumbra={1} /> */}
+                <group position={[10, -100, 10]} rotation={[0, -10, 0]}>
+                    <Model position={[0, 0.25, 0]} url={MODELS[model]} />
+                    <ContactShadows scale={20} blur={10} far={20} />
+                </group>
+                <OrbitControls />
             </Canvas>
         </>
     );
 };
+
+function Model({ url, ...props }: { url: string; position: any }) {
+    const { scene } = useGLTF(url);
+    return <primitive object={scene} {...props} />;
+}
 
 export default App;
