4ecfcae174b2806695b92ce31faa459f601afdab
Fernando Chicaiza <washopilot@yahoo.com>
2022-11-12 16:19:38 -0500

    refactoring and separate MODELS

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -4,41 +4,29 @@ import { Canvas, useThree } from '@react-three/fiber';
 import { button, useControls } from 'leva';
 import { useState, useTransition } from 'react';
 import * as THREE from 'three';
-
 import { GLTF } from 'three-stdlib';
-
-const MODELS: { [key: string]: { url: string; nodes: string[] } } = {
-    ['percha-1']: { url: 'gltf/percha_03_final.glb', nodes: ['elemento-1', 'elemento-2', 'elemento-3'] },
-    ['percha-2']: { url: 'gltf/percha_04_final.glb', nodes: ['elemento-4', 'elemento-5', 'elemento-6'] }
-};
+import { MODELS } from './Models';
 
 type GLTFResult = GLTF & {
     nodes: {
-        [K in typeof MODELS['percha-1']['nodes'][number]]: THREE.Mesh;
+        [name: string]: THREE.Mesh;
     };
     materials: {
-        [K in typeof MODELS['percha-1']['nodes'][number]]: THREE.MeshStandardMaterial;
-    };
-} & {
-    nodes: {
-        [K in typeof MODELS['percha-2']['nodes'][number]]: THREE.Mesh;
-    };
-    materials: {
-        [K in typeof MODELS['percha-2']['nodes'][number]]: THREE.MeshStandardMaterial;
+        [name: string]: THREE.MeshStandardMaterial;
     };
 };
 
 const App = () => {
     const { model } = useControls({
-        model: { value: 'percha-1', options: Object.keys(MODELS), label: 'Mueble' }
+        model: { value: Object.keys(MODELS)[0], options: Object.keys(MODELS), label: 'Mueble' }
     });
 
     return (
         <Canvas gl={{ preserveDrawingBuffer: true }} camera={{ position: [100, 90, 200], fov: 50 }}>
             <group position={[10, -100, 10]} rotation={[0, -10, 0]}>
                 <Model position={[0, 0.25, 0]} url={MODELS[model].url} model={model} />
-                <Env />
             </group>
+            <Env />
             <OrbitControls />
         </Canvas>
     );
@@ -72,8 +60,6 @@ function Env() {
 
 function Model({ url, model, ...props }: { url: string; position: any; model: string }) {
     const { nodes, materials } = useGLTF(url) as unknown as GLTFResult;
-
-    console.log(url, model);
 
     const controls = useControls(
         { ...MODELS[model].nodes.reduce((a, v) => ({ ...a, [v]: { value: '#ffffff' } }), {}) },
---
+++ resources/src/Models.ts
@@ -0,0 +1,4 @@
+export const MODELS: { [key: string]: { url: string; nodes: string[] } } = {
+    ['Estante metálico']: { url: 'gltf/percha_03_final.glb', nodes: ['elemento-1', 'elemento-2', 'elemento-3'] },
+    ['Estante metálico peq']: { url: 'gltf/percha_04_final.glb', nodes: ['elemento-4', 'elemento-5', 'elemento-6'] }
+};
