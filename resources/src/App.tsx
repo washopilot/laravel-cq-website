683416b91ed9ed1fbcdfa6ed2cca3eff64ade2dd
Fernando Chicaiza <washopilot@yahoo.com>
2022-11-30 00:26:38 -0500

    first try siblings comunnicate

--- resources/src/App.tsx
+++ resources/src/App.tsx
@@ -1,13 +1,12 @@
 import { Environment, OrbitControls, useGLTF } from '@react-three/drei';
 import { PresetsType } from '@react-three/drei/helpers/environment-assets';
 import { Canvas, useThree } from '@react-three/fiber';
-import { button, useControls } from 'leva';
-import { useState, useTransition } from 'react';
+import { useEffect, useState, useTransition } from 'react';
 
 import 'react-var-ui/dist/index.css';
 import { GLTF } from 'three-stdlib';
 import { MODELS } from './Models';
-import VarUICustom from './VarUICustom';
+import VarUICustom, { valuesCustomType } from './VarUICustom';
 
 type GLTFResult = GLTF & {
     nodes: {
@@ -19,62 +18,112 @@ type GLTFResult = GLTF & {
 };
 
 const App = () => {
-    const { model } = useControls({
-        model: { value: Object.keys(MODELS)[0], options: Object.keys(MODELS), label: 'Mueble' }
+    // const { model } = useControls({
+    //     model: { value: Object.keys(MODELS)[0], options: Object.keys(MODELS), label: 'Mueble' }
+    // });
+    // const gl = useThree((state) => state.gl);
+
+    const [fireCapture, setfireCapture] = useState(false);
+    const fireCaptureScreen = () => setfireCapture(true);
+
+    const [values, setValues] = useState<valuesCustomType>({
+        selectModel: Object.keys(MODELS)[0],
+        elemento_1: '#FF0000',
+        elemento_2: '#FF0000',
+        elemento_3: '#FF0000',
+        elemento_4: '#FF0000',
+        elemento_5: '#FF0000',
+        elemento_6: '#FF0000',
+        elemento_7: '#FF0000',
+        elemento_8: '#FF0000',
+        elemento_9: '#FF0000',
+        elemento_10: '#FF0000',
+        elemento_11: '#FF0000',
+        elemento_12: '#FF0000',
+        elemento_13: '#FF0000',
+        elemento_14: '#FF0000',
+
+        slider: 0.4,
+        xy: [0, 0.2]
     });
 
     return (
         <>
             <Canvas gl={{ preserveDrawingBuffer: true }} camera={{ position: [100, 90, 200], fov: 50 }}>
                 <group position={[10, -100, 10]} rotation={[0, -190, 0]}>
-                    <Model position={[0, 0.25, 0]} url={MODELS[model].url} model={model} />
+                    <Model
+                        position={[0, 0.25, 0]}
+                        url={MODELS[values.selectModel].url}
+                        model={values.selectModel}
+                        values={values}
+                    />
                 </group>
-                <Env />
+                <Env fireCapture={fireCapture} />
                 <OrbitControls maxPolarAngle={(2 * Math.PI) / 3} minPolarAngle={(1 * Math.PI) / 3} />
             </Canvas>
-            <VarUICustom />
+            <VarUICustom values={values} onSetValues={setValues} captureScreen={fireCaptureScreen} />
         </>
     );
 };
 
-function Env() {
+function Env({ fireCapture }: { fireCapture: boolean }) {
     const [preset, setPreset] = useState<PresetsType>('warehouse');
     const gl = useThree((state) => state.gl);
     // You can use the "inTransition" boolean to react to the loading in-between state,
     // For instance by showing a message
+
+    useEffect(() => {
+        captureScreen();
+    }, [fireCapture]);
+
+    const captureScreen = () => {
+        console.log('function Called');
+    };
+
     const [inTransition, startTransition] = useTransition();
-    const values = useControls({
-        preset: {
-            value: preset,
-            options: ['sunset', 'dawn', 'night', 'warehouse', 'forest', 'apartment', 'studio', 'city', 'park', 'lobby'],
-            // If onChange is present the value will not be reactive, see https://github.com/pmndrs/leva/blob/main/docs/advanced/controlled-inputs.md#onchange
-            // Instead we transition the preset value, which will prevents the suspense bound from triggering its fallback
-            // That way we can hang onto the current environment until the new one has finished loading ...
-            label: 'Ambiente',
-            onChange: (value) => startTransition(() => setPreset(value))
-        },
-        ['capturar pantalla']: button(() => {
-            const link = document.createElement('a');
-            link.setAttribute('download', 'canvas.png');
-            link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'));
-            link.click();
-        })
-    });
+    // const values = useControls({
+    //     preset: {
+    //         value: preset,
+    //         options: ['sunset', 'dawn', 'night', 'warehouse', 'forest', 'apartment', 'studio', 'city', 'park', 'lobby'],
+    //         // If onChange is present the value will not be reactive, see https://github.com/pmndrs/leva/blob/main/docs/advanced/controlled-inputs.md#onchange
+    //         // Instead we transition the preset value, which will prevents the suspense bound from triggering its fallback
+    //         // That way we can hang onto the current environment until the new one has finished loading ...
+    //         label: 'Ambiente',
+    //         onChange: (value) => startTransition(() => setPreset(value))
+    //     },
+    //     ['capturar pantalla']: button(() => {
+    //         const link = document.createElement('a');
+    //         link.setAttribute('download', 'canvas.png');
+    //         link.setAttribute('href', gl.domElement.toDataURL('image/png').replace('image/png', 'image/octet-stream'));
+    //         link.click();
+    //     })
+    // });
+
     return <Environment preset={preset} background />;
 }
 
-function Model({ url, model, ...props }: { url: string; position: any; model: string }) {
+function Model({
+    url,
+    model,
+    values,
+    ...props
+}: {
+    url: string;
+    position: any;
+    model: string;
+    values: valuesCustomType;
+}) {
     const { nodes, materials } = useGLTF(url) as unknown as GLTFResult;
 
-    const controls = useControls(
-        {
-            ...MODELS[model].nodes.reduce(
-                (prev, curr, idx) => ({ ...prev, [curr]: { value: MODELS[model]['colors'][idx] } }),
-                {}
-            )
-        },
-        [nodes]
-    );
+    // const controls = useControls(
+    //     {
+    //         ...MODELS[model].nodes.reduce(
+    //             (prev, curr, idx) => ({ ...prev, [curr]: { value: MODELS[model]['colors'][idx] } }),
+    //             {}
+    //         )
+    //     },
+    //     [nodes]
+    // );
 
     // applyProps(materials['plate'] as unknown as Instance, {
     //     color: plate,
@@ -84,18 +133,20 @@ function Model({ url, model, ...props }: { url: string; position: any; model: st
 
     return (
         <group {...props} dispose={null}>
-            {MODELS[model].nodes.map((item, index) => (
-                <mesh
-                    key={index}
-                    castShadow
-                    receiveShadow
-                    geometry={nodes[item].geometry}
-                    material={materials[item]}
-                    material-color={{ ...(controls as Object) }[item]}
-                    material-roughness={0.1}
-                    material-metalness={0.1}
-                />
-            ))}
+            {MODELS[model].nodes.map((item, index) => {
+                return (
+                    <mesh
+                        key={index}
+                        castShadow
+                        receiveShadow
+                        geometry={nodes[item].geometry}
+                        material={materials[item]}
+                        material-color={values[item.replace('-', '_') as keyof valuesCustomType]}
+                        material-roughness={0.1}
+                        material-metalness={0.1}
+                    />
+                );
+            })}
         </group>
     );
 }
--- resources/src/VarUICustom.tsx
+++ resources/src/VarUICustom.tsx
@@ -1,39 +1,66 @@
-import { useState } from 'react';
+import { useThree } from '@react-three/fiber';
 import { VarButton, VarCategory, VarSelect, VarToggle, VarUI } from 'react-var-ui';
+import { MODELS } from './Models';
 import { VarColorCustom } from './VarColorCustom';
 
-const VarUICustom = () => {
-    const [values, setValues] = useState({
-        toggle: true,
-        color1: '#FF0000',
-        color2: '#FF0000',
-        color3: '#FF0000',
-        select: 1,
-        slider: 0.4,
-        xy: [0, 0.2],
-        string: 'Hello world!'
-    });
+export type valuesCustomType = {
+    selectModel: string;
+    elemento_1: string;
+    elemento_2: string;
+    elemento_3: string;
+    elemento_4: string;
+    elemento_5: string;
+    elemento_6: string;
+    elemento_7: string;
+    elemento_8: string;
+    elemento_9: string;
+    elemento_10: string;
+    elemento_11: string;
+    elemento_12: string;
+    elemento_13: string;
+    elemento_14: string;
+    slider: number;
+    xy: number[];
+};
+
+interface IVarUICustom {
+    values: valuesCustomType;
+    onSetValues: React.Dispatch<React.SetStateAction<valuesCustomType>>;
+    captureScreen: () => void;
+}
+
+const VarUICustom = ({ values, onSetValues, captureScreen }: IVarUICustom) => {
+    // const gl = useThree((state) => state.gl);
 
     return (
-        <VarUI updateValues={setValues} values={values}>
-            <VarCategory label="Example">
-                {/* <VarColor path="color" label="Color" /> */}
-                <VarColorCustom path="color1" label="Color" />
-                <VarColorCustom path="color2" label="Color" />
-                <VarColorCustom path="color3" label="Color" />
-                <VarToggle path="toggle" label="Toggle" />
+        <VarUI updateValues={onSetValues} values={values}>
+            <VarCategory label="Muebles">
                 <VarSelect
-                    path="select"
-                    label="Select"
-                    options={[
-                        { key: 0, label: 'Zero' },
-                        { key: 1, label: 'One' }
-                    ]}
+                    path="selectModel"
+                    label="Tipo"
+                    options={Object.keys(MODELS).map((value, idx) => ({ key: idx, label: value, value: value }))}
                 />
+
+                {values.selectModel == 'Estante metálico' && <VarColorCustom path="elemento_1" label="Elem-1" />}
+                {values.selectModel == 'Estante metálico' && <VarColorCustom path="elemento_2" label="Elem-2" />}
+                {values.selectModel == 'Estante metálico' && <VarColorCustom path="elemento_3" label="Elem-3" />}
+                {values.selectModel == 'Estante metálico peq' && <VarColorCustom path="elemento_4" label="Elem-4" />}
+                {values.selectModel == 'Estante metálico peq' && <VarColorCustom path="elemento_5" label="Elem-5" />}
+                {values.selectModel == 'Estante metálico peq' && <VarColorCustom path="elemento_6" label="Elem-6" />}
+                {values.selectModel == 'Góndola cabecera' && <VarColorCustom path="elemento_7" label="Elem-7" />}
+                {values.selectModel == 'Góndola cabecera' && <VarColorCustom path="elemento_8" label="Elem-8" />}
+                {values.selectModel == 'Góndola cabecera' && <VarColorCustom path="elemento_9" label="Elem-9" />}
+                {values.selectModel == 'Góndola cabecera' && <VarColorCustom path="elemento_10" label="Elem-10" />}
+                {values.selectModel == 'Góndola doble' && <VarColorCustom path="elemento_11" label="Elem-11" />}
+                {values.selectModel == 'Góndola doble' && <VarColorCustom path="elemento_12" label="Elem-12" />}
+                {values.selectModel == 'Góndola doble' && <VarColorCustom path="elemento_13" label="Elem-13" />}
+                {values.selectModel == 'Góndola doble' && <VarColorCustom path="elemento_14" label="Elem-14" />}
+
+                {/* <VarToggle path="toggle" label="Toggle" /> */}
                 {/* <VarSlider label="VarSlider" path="slider" min={0.2} max={0.8} step={0.1} /> */}
                 {/* <VarString label="VarString" path="string" /> */}
                 {/* <VarXY label="VarXY" path="xy" /> */}
-                <VarButton buttonLabel="VarButton" onClick={() => alert('clicked!')} />
+                <VarButton buttonLabel="capturar pantalla" onClick={captureScreen} />
             </VarCategory>
         </VarUI>
     );
