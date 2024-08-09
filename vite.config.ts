977b2b48bb1ad0af58cdc4c6704dfa9cc9ab8f97
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-09 06:42:32 +0000

    add server fix

--- vite.config.ts
+++ vite.config.ts
@@ -11,5 +11,10 @@ export default defineConfig({
             refresh: true
         }),
         tsconfigPaths()
-    ]
+    ],
+    server: {
+        hmr: {
+            host: 'localhost',
+        },
+    }
 });
