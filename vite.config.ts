c854e60a29a80733bece2191adef7164cc522352
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-09 06:27:12 +0000

    remove carets package.json

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite';
+import laravel from 'laravel-vite-plugin';
+import react from '@vitejs/plugin-react';
+import tsconfigPaths from 'vite-tsconfig-paths';
+
+export default defineConfig({
+    plugins: [
+        react(),
+        laravel({
+            input: ['resources/src/main.tsx'],
+            refresh: true
+        }),
+        tsconfigPaths()
+    ]
+});
