d8b46919bebf180ef4ef0b3c7fe1c3fc98f41ade
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-11 16:01:15 +0000

    change db seeder end remove database.sqlite

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -16,13 +16,27 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Crear 5 categorías
-        $categories = Category::factory(3)->create();
+        // Crear 3 categorías
+        $categories = Category::factory(2)->create();
 
-        // Crear 15 productos y asignarlos a las categorías creadas
-        Product::factory(10)->create([
+        // Crear 10 productos y asignarlos a las categorías creadas
+        Product::factory(5)->create([
             'category_id' => fn() => $categories->random()->id,
-        ]);
+        ])->each(function ($product) {
+            // URL de la imagen aleatoria de Picsum
+            $url = 'https://picsum.photos/200/300';
+
+            // Descargar la imagen desde la URL
+            $imageContent = file_get_contents($url);
+
+            // Guardar la imagen en un archivo temporal
+            $tempImagePath = storage_path('app/public/' . uniqid() . '_image.jpg');
+            file_put_contents($tempImagePath, $imageContent);
+
+            // Añadir la imagen al producto utilizando Spatie Media Library
+            $product->addMedia($tempImagePath)->toMediaCollection('products');
+
+        });
 
         // Crear usuario admin
         User::factory()->create([
