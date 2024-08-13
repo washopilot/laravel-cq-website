da60272ea9415606badd8812363288bd5b1f1eaf
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-13 22:43:56 +0000

    render cards first try

--- app/Http/Controllers/ProductsController.php
+++ app/Http/Controllers/ProductsController.php
@@ -3,18 +3,31 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use App\Models\Product; // Asegúrate de importar el modelo Product
+use App\Models\Product;
 use Inertia\Inertia;
 
 class ProductsController extends Controller
 {
     public function index()
     {
-        $products = Product::all(); // Recupera todos los productos
+        $products = Product::all();
         Inertia::setRootView('products.home');
 
         return Inertia::render('AppProducts', [
-            'products' => $products,
+            'products' => $products->map(function ($product) {
+                return [
+                    'id' => $product->id,
+                    'name' => $product->name,
+                    'description' => $product->description,
+                    'price' => $product->price,
+                    'is_visible' => $product->is_visible,
+                    'category_id' => $product->category_id,
+                    'images' => $product->getMedia('products')->map(function ($media) {
+                        return $media->getUrl();
+                    }),
+                ];
+            }),
         ]);
+
     }
 }
--- resources/src/pages/AppProducts.tsx
+++ resources/src/pages/AppProducts.tsx
@@ -1,7 +1,7 @@
-import { Inertia } from '@inertiajs/inertia'
 import './app.css'
+import Card from './Card'
 
-interface Product {
+export interface Product {
     id: number
     name: string
     description: string
@@ -9,6 +9,7 @@ interface Product {
     is_visible: boolean
     category_id: number
     order_column: number
+    images: string[]
 }
 
 interface ProductsProps {
@@ -16,32 +17,22 @@ interface ProductsProps {
 }
 
 const AppProducts = ({ products }: ProductsProps) => {
+    console.log(products)
+
     const sortedProducts = products.sort((a, b) => a.order_column - b.order_column)
 
     return (
-        <div>
-            <h1 className='text-3xl font-bold underline text-yellow-400'>Hello world!</h1>
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Description</th>
-                        <th>Price</th>
-                        <th>Visible</th>
-                    </tr>
-                </thead>
-                <tbody>
+        <>
+            <h1 className='text-3xl font-bold underline text-indigo-500'>Hello world!</h1>
+
+            <div className='min-h-screen bg-gray-100 flex flex-col justify-center'>
+                <div className='relative m-3 flex flex-wrap mx-auto justify-center'>
                     {products.map((product) => (
-                        <tr key={product.id}>
-                            <td>{product.name}</td>
-                            <td>{product.description}</td>
-                            <td>${Number(product.price).toFixed(2)}</td>
-                            <td>{product.is_visible ? 'Yes' : 'No'}</td>
-                        </tr>
+                        <Card key={product.id} product={product} />
                     ))}
-                </tbody>
-            </table>
-        </div>
+                </div>
+            </div>
+        </>
     )
 }
 
