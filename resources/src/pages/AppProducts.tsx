392dc1aa21ce1ff7e156785ee34ff54b8ec183ae
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-15 01:21:03 +0000

    testing modal

--- resources/src/pages/AppProducts.tsx
+++ resources/src/pages/AppProducts.tsx
@@ -1,5 +1,6 @@
-import React from 'react'
-import CardCustom from './CardCustom'
+import { useState } from 'react'
+import CardProduct from './CardProduct'
+import ProductModal from './ProductModal'
 
 export interface Product {
     id: number
@@ -24,6 +25,13 @@ interface ProductsProps {
 }
 
 const AppProducts = ({ products, categories }: ProductsProps) => {
+    const [selectedProduct, setSelectedProduct] = useState<Product>(null!)
+    const [openModal, setOpenModal] = useState(false)
+    const handleProductClick = (product: Product) => {
+        setSelectedProduct(product)
+        setOpenModal(true)
+    }
+
     products.sort((a, b) => a.order_column - b.order_column)
 
     const productsByCategory = categories.map((category) => {
@@ -35,11 +43,11 @@ const AppProducts = ({ products, categories }: ProductsProps) => {
 
     return (
         <div className='bg-white'>
-            <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8'>
+            <div className='mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8 divide-y divide-gray-200'>
                 {productsByCategory.map(({ id, name, products }) => (
-                    <div key={id} className='mb-10'>
-                        <div className='flex items-center justify-between space-x-4'>
-                            <h2 className='text-lg font-medium text-gray-900'>{name}</h2>
+                    <div key={id} className='mb-10 py-5'>
+                        <div className='flex items-center justify-between space-x-4 '>
+                            <h2 className='text-xl font-medium text-gray-900'>{name}</h2>
                             <a
                                 href='#'
                                 className='whitespace-nowrap text-sm font-medium text-indigo-600 hover:text-indigo-500'>
@@ -49,12 +57,17 @@ const AppProducts = ({ products, categories }: ProductsProps) => {
                         </div>
                         <div className='mt-6 grid grid-cols-1 gap-x-8 gap-y-8 sm:grid-cols-2 sm:gap-y-10 lg:grid-cols-4'>
                             {products.map((product) => (
-                                <CardCustom key={product.id} product={product} />
+                                <CardProduct
+                                    key={product.id}
+                                    product={product}
+                                    onButtonClick={() => handleProductClick(product)}
+                                />
                             ))}
                         </div>
                     </div>
                 ))}
             </div>
+            <ProductModal open={openModal} setOpen={setOpenModal} />
         </div>
     )
 }
--- resources/src/pages/CardProduct.tsx
+++ resources/src/pages/CardProduct.tsx
@@ -1,7 +1,12 @@
 import { Product } from './AppProducts'
 
-const CardCustom = ({ product }: { product: Product }) => {
-    function formatCurrency(value: string) {
+type CardProductProps = {
+    product: Product
+    onButtonClick: React.MouseEventHandler<HTMLButtonElement>
+}
+
+const CardProduct = ({ product, onButtonClick }: CardProductProps) => {
+    const formatCurrency = (value: string) => {
         const numberValue = parseFloat(value)
         if (isNaN(numberValue)) {
             return 'Invalid price'
@@ -15,7 +20,7 @@ const CardCustom = ({ product }: { product: Product }) => {
 
     return (
         <div key={product.id} className='group relative'>
-            <div className='aspect-h-3 aspect-w-4 overflow-hidden rounded-lg bg-gray-100'>
+            <div className='aspect-h-4 aspect-w-3 overflow-hidden rounded-lg bg-gray-100'>
                 <img src={product.images[0]} alt={'imageAlt'} className='object-cover object-center' />
                 <div className='flex items-end p-4 opacity-0 group-hover:opacity-100' aria-hidden='true'>
                     <div className='w-full rounded-md bg-white bg-opacity-75 px-4 py-2 text-center text-sm font-medium text-gray-900 backdrop-blur backdrop-filter'>
@@ -25,16 +30,16 @@ const CardCustom = ({ product }: { product: Product }) => {
             </div>
             <div className='mt-4 flex items-center justify-between space-x-8 text-base font-medium text-gray-900'>
                 <h3>
-                    <a href='#'>
+                    <button onClick={onButtonClick}>
                         <span aria-hidden='true' className='absolute inset-0' />
                         {product.name}
-                    </a>
+                    </button>
                 </h3>
-                <p>{product.price}</p>
+                <p>{formatCurrency(product.price)}</p>
             </div>
-            <p className='mt-1 text-sm text-gray-500'>{product.description}</p>
+            <p className='mt-1 text-sm text-gray-500 line-clamp-3'>{product.description}</p>
         </div>
     )
 }
 
-export default CardCustom
+export default CardProduct
