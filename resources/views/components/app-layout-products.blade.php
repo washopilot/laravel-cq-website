88b2923450cab18d0e34611c3b942ac489241366
Fernando Chicaiza <washopilot@yahoo.com>
2024-08-16 22:54:25 -0500

    fix viewport

--- resources/views/components/app-layout-products.blade.php
+++ resources/views/components/app-layout-products.blade.php
@@ -9,7 +9,7 @@
     @vite('resources/src/main-products.tsx')
 
     <meta name="csrf-token" content="{{ csrf_token() }}">
-
+    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
     {{ $slot }}
 
